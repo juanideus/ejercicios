bdf3657a60fa152c7cc9cac4aa736f55dd6c9e50
juanideus <juan.zuniga@alumnos.ucn.cl>
2025-10-06 03:35:59 -0300

    .

--- main.cpp
+++ main.cpp
@@ -6,21 +6,9 @@
 using namespace Ejercicios;
 using namespace std;
 
-//Ejemplos
- // Persona *p=new Persona("Juani",100);
-    // Persona *p2=new Persona("baltazar",90);
-    // Persona *p3=new Persona("benja",0);
-    // Persona *p4=new Persona("gean",1);
-    // ListaSimpleTemplate<Persona> *l=new ListaSimpleTemplate<Persona>();
-    // cout<<l->tostring();
-    // l->insertar(p);
-    // l->insertar(p2);
-    // l->insertar(p3);
-    // l->insertar(p4);
-    // cout<<l->tostring();
 
 
-
+//**Ejecuta aqui tu codigo**
 int main() {
 
 
