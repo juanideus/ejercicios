d623bebcfa606522c082a78424bc22d0a744df8d
poderpasado <baltazar.rodriguezt@gmail.com>
2025-10-21 21:49:44 -0300

    a

--- src/MPP/MPP.h
+++ src/MPP/MPP.h
@@ -4,10 +4,15 @@
 
 #ifndef EJERCICIOS_MPP_H
 #define EJERCICIOS_MPP_H
+#include "NodoMPP.h"
 
 
 class MPP {
-
+    private:
+        NodoMPP** aRow; // lista de listas de filas
+        NodoMPP** aCol; // lista de lisats de columnas
+        int filas;
+        int columnas;
 };
 
 
