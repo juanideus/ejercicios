203d50a4aa67fbf0a205277ca72d436010fe8f7b
poderpasado <baltazar.rodriguezt@gmail.com>
2025-10-31 00:45:19 -0300

    a

--- src/Arboles/ABB/NodoABB.cpp
+++ src/Arboles/ABB/NodoABB.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "NodoABB.h"
+
+template<typename T>
+NodoABB<T>::NodoABB() : value(nullptr), left(nullptr), right(nullptr){}
+
+template<typename T>
+NodoABB<T>::NodoABB(T *value) : value(value), left(nullptr), right(nullptr){}
+
+template<typename T>
+T * NodoABB<T>::getValue() {
+    return this->value;
+}
+
+template<typename T>
+NodoABB<T> * NodoABB<T>::getLeft() {
+    return this->left;
+}
+
+template<typename T>
+NodoABB<T> * NodoABB<T>::getRight() {
+    return this->right;
+}
+
+template<typename T>
+void NodoABB<T>::setValue(T *value) {
+    this->value = value;
+}
+
+template<typename T>
+void NodoABB<T>::setLeft(NodoABB *left) {
+    this->left = left;
+}
+
+template<typename T>
+void NodoABB<T>::setRight(NodoABB *right) {
+    this->right = right;
+}
+
+template<typename T>
+NodoABB<T>::~NodoABB() {
+}
--- src/Arboles/ABB/NodoABB.h
+++ src/Arboles/ABB/NodoABB.h
@@ -1,8 +1,24 @@
 #pragma once
 
-
+template <typename T>
 class NodoABB {
+    private:
+        T* value;
+        NodoABB* left;
+        NodoABB* right;
 
+    public:
+        NodoABB();
+        NodoABB(T* value);
 
+        T* getValue();
+        NodoABB* getLeft();
+        NodoABB* getRight();
+
+        void setValue(T* value);
+        void setLeft(NodoABB* left);
+        void setRight(NodoABB* right);
+
+        ~NodoABB();
 };
 
